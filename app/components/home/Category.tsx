4fe9709de8e4b0b4ed48c90cf24e73279033d7f4
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-11-01 23:37:21 +0600

    category section effect implementation

--- app/components/home/Category.tsx
+++ app/components/home/Category.tsx
@@ -1,11 +1,34 @@
 export const Category = () => {
   return (
     <>
-      <div className="grid grid-cols-4 grid-rows-2 gap-[26px] max-w-[1133px] mx-auto">
-        <img className="row-span-1 col-span-2" src="https://i.ibb.co/wBTMPvz/2-4.png" />
-        <img className="row-span-2 col-span-2" src="https://i.ibb.co/vwv7fRT/5-3.png" />
-        <img className="" src="https://i.ibb.co/9vzbc17/3-5.png" />
-        <img className="" src="https://i.ibb.co/RBgKmwK/4-2.png" />
+      <div className="grid grid-cols-4 grid-rows-2 gap-[26px] items-start justify-center max-w-[1133px] mx-auto mt-[120px]">
+        <div className="row-span-1 col-span-2 relative catImg  ">
+        <img  className="hover:opacity-50 bg-black" src="https://i.ibb.co/wBTMPvz/2-4.png" />
+        <div className="bg-white text-[#22262A] text-xl py-1 px-6 absolute bottom-[27px] left-[30px] hidden">
+            Shirt Fashion
+        </div>
+        </div>
+        <div className="row-span-2 col-span-2 relative catImg  ">
+        <img className="hover:opacity-50 bg-black" src="https://i.ibb.co/vwv7fRT/5-3.png" />
+        <div className="bg-white text-[#22262A] text-xl py-1 px-6 absolute bottom-[27px] left-[30px] hidden">
+            Sweater Fashion
+        </div>
+        </div>
+        <div className="relative catImg  " >
+        <img className="hover:opacity-50 bg-black" src="https://i.ibb.co/9vzbc17/3-5.png" />
+        <div className="bg-white text-[#22262A] text-xl py-1 px-6 absolute bottom-[27px] left-[30px] hidden">
+            Skirt Fashion
+        </div>
+        </div>
+        <div className="relative catImg  ">
+        <img  className="hover:opacity-50 bg-black" src="https://i.ibb.co/RBgKmwK/4-2.png" />
+        <div className="bg-white text-[#22262A] text-xl py-1 px-6 absolute bottom-[27px] left-[30px] hidden">
+            All Women
+        </div>
+        </div>
+        
+        
+        
       </div>
     </>
   );
