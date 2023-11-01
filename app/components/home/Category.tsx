b0b68996ceef5874b906cbc1e3397d7a9574fc2b
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-11-01 23:11:18 +0600

    Category section implementaion

---
+++ app/components/home/Category.tsx
@@ -0,0 +1,12 @@
+export const Category = () => {
+  return (
+    <>
+      <div className="grid grid-cols-4 grid-rows-2 gap-[26px] max-w-[1133px] mx-auto">
+        <img className="row-span-1 col-span-2" src="https://i.ibb.co/wBTMPvz/2-4.png" />
+        <img className="row-span-2 col-span-2" src="https://i.ibb.co/vwv7fRT/5-3.png" />
+        <img className="" src="https://i.ibb.co/9vzbc17/3-5.png" />
+        <img className="" src="https://i.ibb.co/RBgKmwK/4-2.png" />
+      </div>
+    </>
+  );
+};
