a4e61f854f8ff410d5ff07ae1ecd547d54cbcd63
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-12-26 00:11:30 +0600

    brandwork color chaneg

--- app/components/home/BrandWork.tsx
+++ app/components/home/BrandWork.tsx
@@ -7,11 +7,11 @@ const BrandWork = (props: Props) => {
     <div className='bg-[#22262A] py-20 mt-[120px]'>
         <h2 className='text-center text-gray-500 text-7xl font-bold pb-10'>OUR CLIENT</h2>
         <div className='mx-auto max-w-6xl flex justify-between items-center'>
-        <h3 className='text-white font-bold text-4xl'>ZARA</h3>
-        <h3 className='text-white font-bold text-4xl'>SAILOR</h3>
-        <h3 className='text-white font-bold text-4xl'>ARTISAN</h3>
-        <h3 className='text-white font-bold text-4xl'>AARONG</h3>
-        <h3 className='text-white font-bold text-4xl'>DESHAL</h3>
+        <h3 className='text-[#a1a1a1] font-bold text-4xl'>ZARA</h3>
+        <h3 className='text-[#a1a1a1] font-bold text-4xl'>SAILOR</h3>
+        <h3 className='text-[#a1a1a1] font-bold text-4xl'>ARTISAN</h3>
+        <h3 className='text-[#a1a1a1] font-bold text-4xl'>AARONG</h3>
+        <h3 className='text-[#a1a1a1] font-bold text-4xl'>DESHAL</h3>
         </div>
         
     </div>
